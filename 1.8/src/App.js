03ab3260d757720d7cd50eea5ec17ae078d68ee8
anirudh senani <55765373+Anirudh-Senani@users.noreply.github.com>
2021-03-23 16:02:52 +0530

    part 1 Exercise 1.8

---
+++ 1.8/src/App.js
@@ -0,0 +1,63 @@
+import React, {useState} from 'react'
+
+const Display = ({text,counter}) => {
+  return (
+  <div>
+    {text}  {counter}
+  </div>
+  )
+}
+
+const Button = ({text, handleClick}) => {
+  return(
+    <button onClick = {handleClick}>
+      {text}
+    </button>
+  )
+}
+
+const Statistics = ({good, bad,neutral}) => {
+  return(      
+    <div>
+    <h1>Statistics</h1>
+    <Display text = 'good' counter = {good}/>
+    <Display text = 'neutral' counter = {neutral}/>
+    <Display text = 'bad' counter = {bad}/>
+    <Display text = 'average' counter = {(good-bad)/(good+bad+neutral)}/>
+    <Display text = 'positive (%)' counter = {good*100/(good+bad+neutral)}/>
+    </div>
+    )
+}
+
+const App = () => {
+
+  const [good,setGood] = useState(0);
+  const [bad,setBad] = useState(0);
+  const [neutral,setNeutral] = useState(0);
+
+  const handleGood = () => setGood(good+1)
+  
+  const handleNeutral = () => {
+    setNeutral(neutral+1)
+  }
+
+  const handleBad = () => {
+    setBad(bad+1)
+  }
+
+
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <div>
+      <Button text = 'good' handleClick = {handleGood}/>
+      <Button text = 'neutral' handleClick = {handleNeutral}/>
+      <Button text = 'bad'  handleClick = {handleBad}/>
+      </div>
+      <Statistics good = {good} neutral = {neutral} bad = {bad} />
+    </div>
+  )
+}
+
+export default App
