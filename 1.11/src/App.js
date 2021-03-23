9a48f56964f93d6ee3164d2018a7d81bd0d6ec38
anirudh senani <55765373+Anirudh-Senani@users.noreply.github.com>
2021-03-23 16:40:43 +0530

    part 1 Exercise 1.11

---
+++ 1.11/src/App.js
@@ -0,0 +1,83 @@
+import React, {useState} from 'react'
+
+const Display = ({text,counter}) => {
+  return (
+  <div>
+    {text}  {counter}
+  </div>
+  )
+}
+
+const Button = ({text, handleClick}) => {
+  return(
+    <button onClick = {handleClick}>
+      {text}
+    </button>
+  )
+}
+
+const Statistics = ({good, bad,neutral}) => {
+  if (good+bad+neutral === 0){
+    return(<p>No feedback given</p>)
+  }
+  return(      
+    <div>
+    <h1>Statistics</h1>
+    <table>
+    <tr>
+      <td> good </td>
+      <td> {good} </td>
+    </tr>
+    <tr>
+      <td> neutral </td>
+      <td> {neutral} </td>
+    </tr>
+    <tr>
+      <td> bad </td>
+      <td> {bad} </td>
+    </tr>
+    <tr>
+      <td> average </td>
+      <td> {(good-bad)/(good+bad+neutral)} </td>
+    </tr>
+    <tr>
+      <td> positive </td>
+      <td> {good*100/(good+bad+neutral)} % </td>
+    </tr>
+    </table>
+    </div>
+    )
+}
+
+const App = () => {
+
+  const [good,setGood] = useState(0);
+  const [bad,setBad] = useState(0);
+  const [neutral,setNeutral] = useState(0);
+
+  const handleGood = () => setGood(good+1)
+  
+  const handleNeutral = () => {
+    setNeutral(neutral+1)
+  }
+
+  const handleBad = () => {
+    setBad(bad+1)
+  }
+
+
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <div>
+      <Button text = 'good' handleClick = {handleGood}/>
+      <Button text = 'neutral' handleClick = {handleNeutral}/>
+      <Button text = 'bad'  handleClick = {handleBad}/>
+      </div>
+      <Statistics good = {good} neutral = {neutral} bad = {bad} />
+    </div>
+  )
+}
+
+export default App
